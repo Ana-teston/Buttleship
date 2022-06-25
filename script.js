616c4d9a3d86f752108aaa6f4cb87b566e334ed7
Ana-teston <anapaulateston@gmail.com>
2022-06-25 10:47:58 +0200

    firsts function in javaScript test 1

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('testing');
+var view = {
+
+  //this methode take a string message and display it
+  // in the message display area
+
+  displayMessage: function(msg) {
+    var messageArea = document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+
+  displayHit: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "hit")
+  },
+
+  displayMiss: function(location) {
+    var cell = document.getElementById(location);
+    cell.setAttribute("class", "miss")
+  },
+};
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+
+view.displayMessage("Tap tap, is this thing on?");
