62b829f08103d56955bf52f6b67de85f72365787
Ana-teston <anapaulateston@gmail.com>
2022-06-25 16:54:06 +0200

    implementing the model object

--- script.js
+++ script.js
@@ -28,24 +28,44 @@ view.displayHit("26");
 
 view.displayMessage("Tap tap, is this thing on?");
 
+var model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipSunk: 0,
 
+ships: [
+    { locations: ["06", "16", "26"], hits: ["", "", ""] },
+    { locations: ["24", "34", "44"], hits: ["", "", ""] },
+    { locations: ["10", "11", "12"], hits: ["", "", ""] },],
 
-var ships = [
-  { locations: ["31", "41", "51"], hits: ["", "", ""] },
-  { locations: ["14", "24", "34"], hits: ["", "", ""] },
-  { locations: ["00", "01", "02"], hits: ["hit", "", ""] },
-];
+fire: function(guess) {
+  for (var i = 0; i < this.numShips; i++) {
+    var ship = this.ships[1];
+    var locations = ship.locations;
+    var index = locations.indexOf(guess);
+    if (index >= 0) {
+      ship.hits[index] = "hit";
+      view.displayHit(guess);
+      view.displayMessage("HIT");
+      if (this.sunk(ship)) {
+        view.displayMessage("You sank my battleship!");
+        this.sunk++;
+      }
+      return true;
+    }
+  }
+  view.displayMiss(guess);
+  view.displayMessage("you missed.");
+  return false;
+},
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
 
-var ship2 = ships[1];
-var locations = ship2.locations;
-console.log("location is " + locations[1]);
-
-var ship3 = ships[2];
-var hits = ship3.hits;
-if (hits[0] === "hit") {
-  console.log("ouch, hit on third ship at location one");
-} 
-
-var ship1 = ships[0];
-var locations = ship1.locations;
-console.log("location is " + locations[0]);
+};
